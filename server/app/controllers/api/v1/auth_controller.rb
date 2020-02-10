72b729fb9742276598dd613e60047e43f5d18b0f
dut-a <d.dut@live.com>
2020-02-10 12:17:06 -0600

    Done.

--- server/app/controllers/api/v1/auth_controller.rb
+++ server/app/controllers/api/v1/auth_controller.rb
@@ -3,8 +3,10 @@ class Api::V1::AuthController < ApplicationController
 
   def create # POST /api/v1/login
     @user = User.find_by(username: user_login_params[:username])
+    #User#authenticate comes from BCrypt
     # @user.authenticate('password')
     if @user && @user.authenticate(user_login_params[:password])
+      # encode token comes from ApplicationController
       @token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
     else
